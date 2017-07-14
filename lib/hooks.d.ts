2197e02cc56461e3b87c5388dc15aade7ad8e99e
Akos Szokodi <szokodiakos@gmail.com>
2017-07-14 17:31:19 +0200

    added post hook support

--- lib/hooks.d.ts
+++ lib/hooks.d.ts
@@ -1,4 +1,5 @@
 export declare const pre: {
-    <T>(method: string, parallel: boolean, fn: (this: T, next: (err?: Error) => void, done: () => void) => void, errorCb?: (err: Error) => void): (constructor: any) => void;
-    <T>(method: string, fn: (this: T, next: (err?: Error) => void, done: () => void) => void, errorCb?: (err: Error) => void): (constructor: any) => void;
+    <T>(method: "init" | "validate" | "save" | "remove", parallel: boolean, fn: (this: T, next: (err?: Error) => void, done: () => void) => void, errorCb?: (err: Error) => void): (constructor: any) => void;
+    <T>(method: "init" | "validate" | "save" | "remove", fn: (this: T, next: (err?: Error) => void, done: () => void) => void, errorCb?: (err: Error) => void): (constructor: any) => void;
 };
+export declare const post: <T>(method: "init" | "validate" | "save" | "remove", fn: ((doc: T, next: (err?: Error) => void) => void) | ((error: Error, doc: T, next: (err?: Error) => void) => void)) => (constructor: any) => void;
