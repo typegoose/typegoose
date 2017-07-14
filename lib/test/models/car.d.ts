e047da4e5d1bf89375fdab0942fe80ccf4d789d7
Akos Szokodi <szokodiakos@gmail.com>
2017-07-14 17:10:09 +0200

    added pre-hook with tests

--- lib/test/models/car.d.ts
+++ lib/test/models/car.d.ts
@@ -3,5 +3,6 @@ import * as mongoose from 'mongoose';
 import { Typegoose } from '../../typegoose';
 export declare class Car extends Typegoose {
     model: string;
+    isSedan?: boolean;
 }
 export declare const model: mongoose.Model<Car & mongoose.Document> & Car & typeof Car;
--- src/test/models/car.ts
+++ src/test/models/car.ts
@@ -2,12 +2,18 @@ import * as mongoose from 'mongoose';
 
 import { Typegoose, prop, pre } from '../../typegoose';
 
-@pre<Car>('save', (next) => {
+@pre<Car>('save', function(next) {
+  if (this.model === 'Trabant') {
+    this.isSedan = true;
+  }
   next();
 })
 export class Car extends Typegoose {
   @prop({ required: true })
   model: string;
+
+  @prop()
+  isSedan?: boolean;
 }
 
 export const model = new Car().getModelForClass(Car);
