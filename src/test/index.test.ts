145f825d57427581b90ad8abec0dfc59b6079b92
Akos Szokodi <szokodiakos@gmail.com>
2017-07-15 08:59:10 +0200

    removed depretaced mongoose connect api

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -13,9 +13,11 @@ import { Genders } from './enums/genders';
 
 const MONGO_PORT = process.env.MONGO_PORT || 27017;
 
+const connectionOptions = { useMongoClient: true } as mongoose.ConnectionOptions;
 const connect = () =>
   new Promise((resolve, reject) =>
-    mongoose.connect(`mongodb://localhost:${MONGO_PORT}/typegoosetest`, (err) => err ? reject(err) : resolve()));
+    mongoose.connect(`mongodb://localhost:${MONGO_PORT}/typegoosetest`, connectionOptions, (err) =>
+      err ? reject(err) : resolve()));
 
 const initDatabase = () =>
   connect().then(() => mongoose.connection.db.dropDatabase());
