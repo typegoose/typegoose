0923d5ac4b0f12eb2a9fd6f8cdba7c613e1e2ee1
Vincze Dani <vdani@codingsans.com>
2017-09-19 18:12:58 +0200

    hook typings inproved so this reflects it better

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,24 +1,52 @@
-import { MongooseDocument } from 'mongoose';
+import { MongooseDocument, Query } from 'mongoose';
 
 import { hooks as hooksData } from './data';
 
-type Method = 'init' | 'validate' | 'save' | 'remove' |
-'count' | 'find' | 'findOne' | 'findOneAndRemove' | 'findOneAndUpdate' | 'update'|
-'insertMany';
+type DocumentMethod = 'init' | 'validate' | 'save' | 'remove';
+type QueryMethod = 'count' | 'find' | 'findOne' | 'findOneAndRemove' | 'findOneAndUpdate' | 'update';
+type ModelMethod = 'insertMany';
+
 type ClassDecorator = (constructor: any) => void;
 type HookNextFn = (err?: Error) => void;
 
 type PreDoneFn = () => void;
-type PreFn<T> = (this: T & MongooseDocument, next: HookNextFn, done: PreDoneFn) => void;
 type PreErrorCb = (err: Error) => void;
 
-type PostFn<T> = (doc: T & MongooseDocument, next: HookNextFn) => void;
-type PostFnWithError<T> = (error: Error, doc: T & MongooseDocument, next: HookNextFn) => void;
+type TypegooseDoc<T> = T & MongooseDocument;
+
+type DocumentPreSerialFn<T> = (this: TypegooseDoc<T>, next: HookNextFn) => void;
+type DocumentPreParallelFn<T> = (this: TypegooseDoc<T>, next: HookNextFn, done: PreDoneFn) => void;
+
+type QueryPreSerialFn<T> = (this: Query<T>, next: HookNextFn) => void;
+type QueryPreParallelFn<T> = (this: Query<T>, next: HookNextFn, done: PreDoneFn) => void;
+
+// TODO the this refers to the Model, I need help with this
+type ModelPreSerialFn<T> = (this: any, next: HookNextFn) => void;
+type ModelPreParallelFn<T> = (this: any, next: HookNextFn, done: PreDoneFn) => void;
+
+// this depends on the hook method
+type PostResult<T> = TypegooseDoc<T> | TypegooseDoc<T>[] | number;
+
+type DocumentPostFn<T> = (this: TypegooseDoc<T>, doc: TypegooseDoc<T>, next?: HookNextFn) => void;
+type QueryPostFn<T> = (this: Query<T>, result: PostResult<T>, next?: HookNextFn) => void;
+type ModelPostFn<T> = (this: any, result: any, next?: HookNextFn) => void;
+
+type PostFn<T> = (result: PostResult<T>, next: HookNextFn) => void;
+type PostFnWithError<T> = (error: Error, result: PostResult<T>, next: HookNextFn) => void;
 
 interface Hooks {
-  pre<T>(method: Method, parallel: boolean, fn: PreFn<T>, errorCb?: PreErrorCb): ClassDecorator;
-  pre<T>(method: Method, fn: PreFn<T>, errorCb?: PreErrorCb): ClassDecorator;
-  post<T>(method: Method, fn: PostFn<T> | PostFnWithError<T>): ClassDecorator;
+  pre<T>(method: DocumentMethod, fn: DocumentPreSerialFn<T>, errorCb?: PreErrorCb): ClassDecorator;
+  pre<T>(method: DocumentMethod, parallel: boolean, fn: DocumentPreParallelFn<T>, errorCb?: PreErrorCb): ClassDecorator;
+
+  pre<T>(method: QueryMethod, fn: QueryPreSerialFn<T>, errorCb?: PreErrorCb): ClassDecorator;
+  pre<T>(method: QueryMethod, parallel: boolean, fn: QueryPreParallelFn<T>, errorCb?: PreErrorCb): ClassDecorator;
+
+  pre<T>(method: ModelMethod, fn: ModelPreSerialFn<T>, errorCb?: PreErrorCb): ClassDecorator;
+  pre<T>(method: ModelMethod, parallel: boolean, fn: ModelPreParallelFn<T>, errorCb?: PreErrorCb): ClassDecorator;
+
+  post<T>(method: DocumentMethod, fn: DocumentPostFn<T> | PostFnWithError<T>): ClassDecorator;
+  post<T>(method: QueryMethod, fn: QueryPostFn<T> | PostFnWithError<T>): ClassDecorator;
+  post<T>(method: ModelMethod, fn: ModelPostFn<T> | PostFnWithError<T>): ClassDecorator;
 }
 
 const hooks: Hooks = {
