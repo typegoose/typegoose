481095e17380670951f41d0d9d4574ed27c33e53
Vincze Dani <vdani@codingsans.com>
2017-09-21 10:47:17 +0200

    Revert "travis script change"
    This reverts commit 47270a48146580fc8477a93d20183c4c9391c882.

---
+++ lib/errors.d.ts
@@ -0,0 +1,12 @@
+export declare class InvalidPropError extends Error {
+    constructor(typeName: any, key: string);
+}
+export declare class NotNumberTypeError extends Error {
+    constructor(key: string);
+}
+export declare class NotStringTypeError extends Error {
+    constructor(key: string);
+}
+export declare class NoMetadataError extends Error {
+    constructor(key: string);
+}
---
+++ lib/method.d.ts
@@ -0,0 +1,2 @@
+export declare const staticMethod: (target: any, key: string, descriptor: TypedPropertyDescriptor<any>) => void;
+export declare const instanceMethod: (target: any, key: string, descriptor: TypedPropertyDescriptor<any>) => void;
