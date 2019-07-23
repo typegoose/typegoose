c31be73d7acd870fbf690ac79b27778b8578123c
hasezoey <hasezoey@gmail.com>
2019-07-23 21:06:17 +0200

    Add public buildSchema method with TSDoc (#341)
    - add a public method for buildSchema
    - add tsdoc for public-buildSchema & private _buildSchema

--- src/typegoose.ts
+++ src/typegoose.ts
@@ -44,17 +44,7 @@ export class Typegoose {
   ) {
     const name = this.constructor.name;
 
-    // get schema of current model
-    let sch = this.buildSchema<T>(t, name, schemaOptions);
-    // get parents class name
-    let parentCtor = Object.getPrototypeOf(this.constructor.prototype).constructor;
-    // iterate trough all parents
-    while (parentCtor && parentCtor.name !== 'Typegoose' && parentCtor.name !== 'Object') {
-      // extend schema
-      sch = this.buildSchema<T>(t, parentCtor.name, schemaOptions, sch);
-      // next parent
-      parentCtor = Object.getPrototypeOf(parentCtor.prototype).constructor;
-    }
+    const sch = this.buildSchema<T>(t, { existingMongoose, schemaOptions });
 
     let model = mongoose.model.bind(mongoose);
     if (existingConnection) {
@@ -69,68 +59,104 @@ export class Typegoose {
     return models[name] as ModelType<this> & T;
   }
 
-  private buildSchema<T>(t: T, name: string, schemaOptions: any, sch?: mongoose.Schema) {
-    const Schema = mongoose.Schema;
+  /**
+   * Generates a Mongoose schema out of class props, iterating through all parents
+   * @param t The not initialized Class
+   * @param schemaOptions Options for the Schema
+   * @returns Returns the Build Schema
+   */
+  public buildSchema<T>(t: T, { schemaOptions }: GetModelForClassOptions = {}) {
+    const name = this.constructor.name;
 
-    if (!sch) {
-      sch = schemaOptions ? new Schema(schema[name], schemaOptions) : new Schema(schema[name]);
-    } else {
-      sch.add(schema[name]);
+    // get schema of current model
+    let sch = _buildSchema<T>(t, name, schemaOptions);
+    /** Parent Constructor */
+    let parentCtor = Object.getPrototypeOf(this.constructor.prototype).constructor;
+    // iterate trough all parents
+    while (parentCtor && parentCtor.name !== 'Typegoose' && parentCtor.name !== 'Object') {
+      // extend schema
+      sch = _buildSchema<T>(t, parentCtor.name, schemaOptions, sch);
+      // next parent
+      parentCtor = Object.getPrototypeOf(parentCtor.prototype).constructor;
     }
+    return sch;
+  }
+}
 
-    const staticMethods = methods.staticMethods[name];
-    if (staticMethods) {
-      sch.statics = Object.assign(staticMethods, sch.statics || {});
-    } else {
-      sch.statics = sch.statics || {};
+/**
+ * Private schema builder out of class props
+ * -> If you discover this, dont use this function, use Typegoose.buildSchema!
+ * @param t The not initialized Class
+ * @param name The Name to save the Schema Under (Mostly Constructor.name)
+ * @param schemaOptions Options for the Schema
+ * @param sch Already Existing Schema?
+ * @returns Returns the Build Schema
+ * @private
+ */
+function _buildSchema<T>(t: T, name: string, schemaOptions: any, sch?: mongoose.Schema) {
+  /** Simplify the usage */
+  const Schema = mongoose.Schema;
+
+  if (!sch) {
+    sch = schemaOptions ? new Schema(schema[name], schemaOptions) : new Schema(schema[name]);
+  } else {
+    sch.add(schema[name]);
+  }
+
+  /** Simplify the usage */
+  const staticMethods = methods.staticMethods[name];
+  if (staticMethods) {
+    sch.statics = Object.assign(staticMethods, sch.statics || {});
+  } else {
+    sch.statics = sch.statics || {};
+  }
+
+  /** Simplify the usage */
+  const instanceMethods = methods.instanceMethods[name];
+  if (instanceMethods) {
+    sch.methods = Object.assign(instanceMethods, sch.methods || {});
+  } else {
+    sch.methods = sch.methods || {};
+  }
+
+  if (hooks[name]) { // checking to just dont get errors like "hooks[name].pre is not defined"
+    hooks[name].pre.forEach(preHookArgs => {
+      (sch as any).pre(...preHookArgs);
+    });
+    hooks[name].post.forEach(postHookArgs => {
+      (sch as any).post(...postHookArgs);
+    });
+  }
+
+  if (plugins[name]) { // same as the "if (hooks[name])"
+    for (const plugin of plugins[name]) {
+      sch.plugin(plugin.mongoosePlugin, plugin.options);
     }
+  }
 
-    const instanceMethods = methods.instanceMethods[name];
-    if (instanceMethods) {
-      sch.methods = Object.assign(instanceMethods, sch.methods || {});
-    } else {
-      sch.methods = sch.methods || {};
-    }
+  /** Simplify the usage */
+  const getterSetters = virtuals[name];
+  if (getterSetters) {
+    for (const key of Object.keys(getterSetters)) {
+      if (getterSetters[key].options && getterSetters[key].options.overwrite) {
+        sch.virtual(key, getterSetters[key].options);
+      } else {
+        if (getterSetters[key].get) {
+          sch.virtual(key, getterSetters[key].options).get(getterSetters[key].get);
+        }
 
-    if (hooks[name]) {
-      const preHooks = hooks[name].pre;
-      preHooks.forEach(preHookArgs => {
-        (sch as any).pre(...preHookArgs);
-      });
-      const postHooks = hooks[name].post;
-      postHooks.forEach(postHookArgs => {
-        (sch as any).post(...postHookArgs);
-      });
-    }
-
-    if (plugins[name]) {
-      for (const plugin of plugins[name]) {
-        sch.plugin(plugin.mongoosePlugin, plugin.options);
-      }
-    }
-
-    const getterSetters = virtuals[name];
-    if (getterSetters) {
-      for (const key of Object.keys(getterSetters)) {
-        if (getterSetters[key].options && getterSetters[key].options.overwrite) {
-          sch.virtual(key, getterSetters[key].options);
-        } else {
-          if (getterSetters[key].get) {
-            sch.virtual(key, getterSetters[key].options).get(getterSetters[key].get);
-          }
-
-          if (getterSetters[key].set) {
-            sch.virtual(key, getterSetters[key].options).set(getterSetters[key].set);
-          }
+        if (getterSetters[key].set) {
+          sch.virtual(key, getterSetters[key].options).set(getterSetters[key].set);
         }
       }
     }
-
-    const indices = Reflect.getMetadata('typegoose:indices', t) || [];
-    for (const index of indices) {
-      sch.index(index.fields, index.options);
-    }
-
-    return sch;
   }
+
+  /** Get Metadata for indices */
+  const indices = Reflect.getMetadata('typegoose:indices', t) || [];
+  for (const index of indices) {
+    sch.index(index.fields, index.options);
+  }
+
+  return sch;
 }
