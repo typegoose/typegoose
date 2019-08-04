2e1655f8aadfe71bfa597509af0ece80bd447c19
hasezoey <hasezoey@gmail.com>
2019-08-04 17:31:14 +0200

    Fix the double typeguards - removes the typeguards from utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -86,19 +86,3 @@ export function getClassForDocument(document: mongoose.Document): any {
   const modelName = (document.constructor as mongoose.Model<typeof document>).modelName;
   return constructors[modelName];
 }
-
-/**
- * Check if the given document is already populated
- * @param doc The Ref with uncertain type
- */
-export function isDocument<T>(doc: Ref<T>): doc is InstanceType<T> {
-  return doc instanceof mongoose.Model;
-}
-
-/**
- * Check if the given array is already populated
- * @param docs The Array of Refs with uncertain type
- */
-export function isDocumentArray<T>(docs: Ref<T>[]): docs is InstanceType<T>[] {
-  return Array.isArray(docs) && docs.every(v => isDocument(v));
-}
