0b736590f0dca7aae0949cba3069100c2c2308ff
hasezoey <hasezoey@gmail.com>
2019-08-29 12:45:44 +0200

    Fix Github-Pages "/typedoc"

--- ghPagesPre.sh
+++ ghPagesPre.sh
@@ -7,6 +7,8 @@ npm run doc
 mv doc/* github-page/typedoc/
 find . -not -regex "^\.\/doc.*\|^\.\/github-page.*\|^\.\/\.git.*" -delete
 mv github-page/* ./
+rm .gitignore
+mv travis-.gitignore .gitignore # because "typedoc" folder should be ignored on normal pushed, but not by travis
 
 # redundant, just to have it clean
 rm -rf github-page
