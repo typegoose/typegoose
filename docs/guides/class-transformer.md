68f4b3af3a7a31f03251db3c759c51aa9f820a8b
Roms1383 <romain.kelifa@gmail.com>
2020-10-20 19:14:42 +0700

    Update from code review

---
+++ docs/guides/class-transformer.md
@@ -0,0 +1,73 @@
+---
+id: using-with-class-transformer
+title: 'Using with class-transformer'
+---
+
+This guide shows how to use `typegoose` with `class-transformer`.
+
+## Implementation
+
+Suppose you have this `Account` class decorated with `class-transformer` :
+```ts
+import { classToPlain, Exclude, Expose, plainToClass, Transform } from 'class-transformer';
+
+import { getModelForClass, mongoose, prop } from 'typegoose';
+
+// re-implement base Document to allow class-transformer to serialize/deserialize _id and __v
+class Document {
+  @Expose()
+  @Transform(
+    // deserialize ObjectId into a string 
+    (value: any) => value instanceof mongoose.Types.ObjectId
+    ? value.toHexString()
+    : value,
+    { toClassOnly: true })
+  public _id: string
+  
+  @Expose()
+  public __v: number
+}
+
+@Exclude()
+class Account extends Document {
+  @prop()
+  @Expose()
+  public email: string
+
+  @prop()
+  @Expose({ groups: ['admin'] })
+  password: string;
+}
+
+const AccountModel = getModelForClass(Account)
+```
+
+You can then use, for example :
+
+* `lean()` on the query :
+  ```ts
+  // lean return a Plain Old Javascript Object
+  const pojo = await AccountModel.findById(id).orFail().lean()
+  // deserialize Plain Old Javascript Object into an instance of the Account class
+  const deserialized = plainToClass(Account, pojo)
+  // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
+  const serialized = classToPlain(deserialized)
+  ```
+
+* `exec()` on the query :
+  ```ts
+  // exec return a Mongoose Object
+  const mo = await AccountModel.findById(id).orFail().exec()
+  // deserialize Mongoose Object into an instance of the Account class
+  const deserialized = plainToClass(Account, mo)
+  // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
+  const serialized = classToPlain(deserialized)
+  ```
+
+As you can see from these examples, there is :
+* a redundant step to first turn the output of the query into a full instance of `Account` : `plainToClass(..., ...)`
+* before being able to benefit from its features for serialization : `classToPlain(...)`
+
+Reason is that queries will output `DocumentType<Account>` instead of required `Account` in this example.
+
+`class-transformer` can only operates its magic on instances of annotated class.
--- test/tests/classTransformer.test.ts
+++ test/tests/classTransformer.test.ts
@@ -1,15 +1,13 @@
-import { ObjectId } from 'bson';
 import { classToPlain, Exclude, Expose, plainToClass, Transform } from 'class-transformer';
 
-import { prop } from '../../src/prop';
-import { getModelForClass } from '../../src/typegoose';
+import { getModelForClass, mongoose, prop } from '../../src/typegoose';
 
 // re-implement base Document to allow class-transformer to serialize/deserialize its properties
 class Document {
   @Expose()
   @Transform(
     // makes sure that when deserializing from a Mongoose Object, ObjectId is serialized into a string 
-    (value: any) => value instanceof ObjectId
+    (value: any) => value instanceof mongoose.Types.ObjectId
     ? value.toHexString()
     : value,
     { toClassOnly: true })
@@ -46,9 +44,9 @@ describe('class-transformer', () => {
   describe('lean', () => {
     it('should be able to use class-transformer', async () => {
       // lean return a Plain Old Javascript Object
-      const POJO = await AccountModel.findById(id).lean()
+      const pojo = await AccountModel.findById(id).orFail().lean()
       // deserialize Plain Old Javascript Object into an instance of the Account class
-      const deserialized = plainToClass(Account, POJO)
+      const deserialized = plainToClass(Account, pojo)
       // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
       const serialized = classToPlain(deserialized)
       // the reason for doing a transformation round-trip here
@@ -62,10 +60,10 @@ describe('class-transformer', () => {
     })
     it(`should be able to use class-transformer's groups`, async () => {
       // lean return a Plain Old Javascript Object
-      const POJO = await AccountModel.findById(id).lean()
+      const pojo = await AccountModel.findById(id).orFail().lean()
       const options = { groups: ['admin'] }
       // deserialize Plain Old Javascript Object into an instance of the Account class
-      const deserialized = plainToClass(Account, POJO, options)
+      const deserialized = plainToClass(Account, pojo, options)
       // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
       const serialized = classToPlain(deserialized, options)
       // the reason for doing a transformation round-trip here
@@ -82,9 +80,9 @@ describe('class-transformer', () => {
   describe('exec', () => {
     it('should be able to use class-transformer', async () => {
       // exec return a Mongoose Object
-      const MO = await AccountModel.findById(id).exec()
+      const mo = await AccountModel.findById(id).orFail().exec()
       // deserialize Mongoose Object into an instance of the Account class
-      const deserialized = plainToClass(Account, MO)
+      const deserialized = plainToClass(Account, mo)
       // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
       const serialized = classToPlain(deserialized)
       // the reason for doing a transformation round-trip here
@@ -98,10 +96,10 @@ describe('class-transformer', () => {
     })
     it(`should be able to use class-transformer's groups`, async () => {
       // exec return a Mongoose Object
-      const MO = await AccountModel.findById(id).exec()
+      const mo = await AccountModel.findById(id).orFail().exec()
       const options = { groups: ['admin'] }
       // deserialize Mongoose Object into an instance of the Account class
-      const deserialized = plainToClass(Account, MO, options)
+      const deserialized = plainToClass(Account, mo, options)
       // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
       const serialized = classToPlain(deserialized, options)
       // the reason for doing a transformation round-trip here
