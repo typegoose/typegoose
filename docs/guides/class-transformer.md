b4b1a9e2411c0c69a44332de60d5f79d6d321d29
hasezoey <hasezoey@gmail.com>
2020-10-27 11:24:34 +0100

    apply style changes
    - apply tslint suggestions
    - apply markdown suggestions (vscode extension)
    -  change some texts for more context

--- docs/guides/class-transformer.md
+++ docs/guides/class-transformer.md
@@ -7,67 +7,70 @@ This guide shows how to use `typegoose` with `class-transformer`.
 
 ## Implementation
 
-Suppose you have this `Account` class decorated with `class-transformer` :
-```ts
-import { classToPlain, Exclude, Expose, plainToClass, Transform } from 'class-transformer';
+Suppose you have this `Account` class decorated with `class-transformer`:
 
+```ts
+import { Exclude, Expose, Transform } from 'class-transformer';
 import { getModelForClass, mongoose, prop } from 'typegoose';
 
 // re-implement base Document to allow class-transformer to serialize/deserialize _id and __v
-class Document {
+class DocumentCT {
   @Expose()
   @Transform(
-    // deserialize ObjectId into a string 
+    // deserialize ObjectId into a string
     (value: any) => value instanceof mongoose.Types.ObjectId
-    ? value.toHexString()
-    : value,
+      ? value.toHexString()
+      : value,
     { toClassOnly: true })
-  public _id: string
-  
+  public _id: string;
+
   @Expose()
-  public __v: number
+  public __v: number;
 }
 
 @Exclude()
-class Account extends Document {
+class Account extends DocumentCT {
   @prop()
   @Expose()
-  public email: string
+  public email: string;
 
   @prop()
   @Expose({ groups: ['admin'] })
-  password: string;
+  public password: string;
 }
 
-const AccountModel = getModelForClass(Account)
+const AccountModel = getModelForClass(Account);
 ```
 
-You can then use, for example :
+You can then use, for example:
 
-* `lean()` on the query :
+* `lean()` on the query:
+
   ```ts
   // lean return a Plain Old Javascript Object
-  const pojo = await AccountModel.findById(id).orFail().lean()
+  const pojo = await AccountModel.findById(id).orFail().lean().exec();
   // deserialize Plain Old Javascript Object into an instance of the Account class
-  const deserialized = plainToClass(Account, pojo)
+  const deserialized = plainToClass(Account, pojo);
   // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
-  const serialized = classToPlain(deserialized)
+  const serialized = classToPlain(deserialized);
   ```
 
-* `exec()` on the query :
+* or an normal document:
+
   ```ts
   // exec return a Mongoose Object
-  const mo = await AccountModel.findById(id).orFail().exec()
+  const mo = await AccountModel.findById(id).orFail().exec();
   // deserialize Mongoose Object into an instance of the Account class
-  const deserialized = plainToClass(Account, mo)
+  const deserialized = plainToClass(Account, mo);
   // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
-  const serialized = classToPlain(deserialized)
+  const serialized = classToPlain(deserialized);
   ```
 
-As you can see from these examples, there is :
-* a redundant step to first turn the output of the query into a full instance of `Account` : `plainToClass(..., ...)`
-* before being able to benefit from its features for serialization : `classToPlain(...)`
+As you can see from these examples, there is:
 
-Reason is that queries will output `DocumentType<Account>` instead of required `Account` in this example.
+* a redundant step to first turn the output of the query into a full instance of `Account` : `plainToClass(..., ...)`
+* before being able to benefit from its features for serialization: `classToPlain(...)`
+
+Reason is that queries will output `DocumentType<Account>` (Mongoose Document) instead of required `Account` (Plain Object / instance of the Class) in this example.
 
 `class-transformer` can only operates its magic on instances of annotated class.
--- test/tests/classTransformer.test.ts
+++ test/tests/classTransformer.test.ts
@@ -3,114 +3,111 @@ import { classToPlain, Exclude, Expose, plainToClass, Transform } from 'class-tr
 import { getModelForClass, mongoose, prop } from '../../src/typegoose';
 
 // re-implement base Document to allow class-transformer to serialize/deserialize its properties
-class Document {
+class DocumentCT {
   @Expose()
   @Transform(
-    // makes sure that when deserializing from a Mongoose Object, ObjectId is serialized into a string 
+    // makes sure that when deserializing from a Mongoose Object, ObjectId is serialized into a string
     (value: any) => value instanceof mongoose.Types.ObjectId
-    ? value.toHexString()
-    : value,
+      ? value.toHexString()
+      : value,
     { toClassOnly: true })
-  public _id: string
-  
+  public _id: string;
+
   @Expose()
-  public __v: number
+  // tslint:disable-next-line:variable-name
+  public __v: number;
 }
 
 @Exclude()
-class Account extends Document {
+class Account extends DocumentCT {
   @prop()
   @Expose()
-  public email: string
+  public email: string;
 
   @prop()
   @Expose({ groups: ['admin'] })
-  password: string;
+  public password: string;
 }
 
-const AccountModel = getModelForClass(Account)
+const AccountModel = getModelForClass(Account);
 
 describe('class-transformer', () => {
-  let id: string
+  let id: string;
   beforeAll(async () => {
     const { _id } = await AccountModel.create({
       email: 'somebody@gmail.com',
-      password: 'secret',
-    } as Account)
+      password: 'secret'
+    } as Account);
     // note here that _id is an ObjectId, hence the toString()
     // otherwise it will have the shape of : { _bsonType: 'ObjectId', id: ArrayBuffer }
-    id = _id.toString()
-  })
-  describe('lean', () => {
+    id = _id.toString();
+  });
+  describe('lean query', () => {
     it('should be able to use class-transformer', async () => {
       // lean return a Plain Old Javascript Object
-      const pojo = await AccountModel.findById(id).orFail().lean()
+      const pojo = await AccountModel.findById(id).orFail().lean().exec();
       // deserialize Plain Old Javascript Object into an instance of the Account class
-      const deserialized = plainToClass(Account, pojo)
+      const deserialized = plainToClass(Account, pojo);
       // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
-      const serialized = classToPlain(deserialized)
+      const serialized = classToPlain(deserialized);
       // the reason for doing a transformation round-trip here
       // is that class-transformer can only works it magic on an instance of a class with its annotation
-      expect(serialized)
-      .toStrictEqual({
+      expect(serialized).toStrictEqual({
         _id: id,
         __v: 0,
-        email: 'somebody@gmail.com',
-      })
-    })
+        email: 'somebody@gmail.com'
+      });
+    });
     it(`should be able to use class-transformer's groups`, async () => {
       // lean return a Plain Old Javascript Object
-      const pojo = await AccountModel.findById(id).orFail().lean()
-      const options = { groups: ['admin'] }
+      const pojo = await AccountModel.findById(id).orFail().lean().exec();
+      const options = { groups: ['admin'] };
       // deserialize Plain Old Javascript Object into an instance of the Account class
-      const deserialized = plainToClass(Account, pojo, options)
+      const deserialized = plainToClass(Account, pojo, options);
       // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
-      const serialized = classToPlain(deserialized, options)
+      const serialized = classToPlain(deserialized, options);
       // the reason for doing a transformation round-trip here
       // is that class-transformer can only works it magic on an instance of a class with its annotation
-      expect(serialized)
-      .toStrictEqual({
+      expect(serialized).toStrictEqual({
         _id: id,
         __v: 0,
         email: 'somebody@gmail.com',
-        password: 'secret',
-      })
-    })
-  })
-  describe('exec', () => {
+        password: 'secret'
+      });
+    });
+  });
+  describe('normal query', () => {
     it('should be able to use class-transformer', async () => {
       // exec return a Mongoose Object
-      const mo = await AccountModel.findById(id).orFail().exec()
+      const doc = await AccountModel.findById(id).orFail().exec();
       // deserialize Mongoose Object into an instance of the Account class
-      const deserialized = plainToClass(Account, mo)
+      const deserialized = plainToClass(Account, doc);
       // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
-      const serialized = classToPlain(deserialized)
+      const serialized = classToPlain(deserialized);
       // the reason for doing a transformation round-trip here
       // is that class-transformer can only works it magic on an instance of a class with its annotation
-      expect(serialized)
-      .toStrictEqual({
+      expect(serialized).toStrictEqual({
         _id: id,
         __v: 0,
-        email: 'somebody@gmail.com',
-      })
-    })
+        email: 'somebody@gmail.com'
+      });
+    });
     it(`should be able to use class-transformer's groups`, async () => {
       // exec return a Mongoose Object
-      const mo = await AccountModel.findById(id).orFail().exec()
-      const options = { groups: ['admin'] }
+      const doc = await AccountModel.findById(id).orFail().exec();
+      const options = { groups: ['admin'] };
       // deserialize Mongoose Object into an instance of the Account class
-      const deserialized = plainToClass(Account, mo, options)
+      const deserialized = plainToClass(Account, doc, options);
       // serialize Account instance back to a Plain Old Javascript Object, applying class-transformer's magic
-      const serialized = classToPlain(deserialized, options)
+      const serialized = classToPlain(deserialized, options);
       // the reason for doing a transformation round-trip here
       // is that class-transformer can only works it magic on an instance of a class with its annotation
-      expect(serialized)
-      .toStrictEqual({
+      expect(serialized).toStrictEqual({
         _id: id,
         __v: 0,
         email: 'somebody@gmail.com',
-        password: 'secret',
-      })
-    })
-  })
-})
+        password: 'secret'
+      });
+    });
+  });
+});
