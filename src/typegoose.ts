23c6f7fe7c693f9d78fee681fa7488ddf701109e
Tristan Lohman <gatorcse@gmail.com>
2020-12-29 08:22:25 -0800

    fix(typegoose): mongoose version diagnostic info. (#458)

--- src/typegoose.ts
+++ src/typegoose.ts
@@ -9,7 +9,7 @@ import { assertion, assertionIsClass, getName, isNullOrUndefined, mergeMetadata,
 if (!isNullOrUndefined(process?.version) && !isNullOrUndefined(mongoose?.version)) { // for usage on client side
   /* istanbul ignore next */
   if (semver.lt(mongoose?.version, '5.9.14')) {
-    throw new Error('Please use mongoose 5.9.14 or higher [E001]');
+    throw new Error(`Please use mongoose 5.9.14 or higher (Current mongoose: ${mongoose.version}) [E001]`);
   }
 
   /* istanbul ignore next */
