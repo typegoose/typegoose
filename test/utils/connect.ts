857102e8de2065799a02f281cf207a05b2ac05c1
hasezoey <hasezoey@gmail.com>
2021-07-29 14:52:37 +0200

    test(utils/connect): use new "asPromise" for "createConnection"

--- test/utils/connect.ts
+++ test/utils/connect.ts
@@ -35,7 +35,7 @@ export async function connect(extraConfig: ExtraConnectionConfig = {}): Promise<
   const connectionString = `${process.env.MONGO_URI}/${extraConfig.dbName ?? config.DataBase}`;
 
   if (extraConfig.createNewConnection) {
-    connection = mongooseInstance.createConnection(connectionString, options);
+    connection = await mongooseInstance.createConnection(connectionString, options).asPromise();
   } else {
     await mongoose.connect(connectionString, options);
     connection = mongooseInstance.connection;
