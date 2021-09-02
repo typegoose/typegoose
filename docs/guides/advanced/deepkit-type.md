70e95d01746164645c9733fdf106f3d5e9143663
Scott Molinari <scott.molinari@adduco.de>
2021-09-02 07:07:29 +0200

    feat: adds tests and docs for @typekit/type

---
+++ docs/guides/advanced/deepkit-type.md
@@ -0,0 +1,93 @@
+---
+id: using-with-deepkit-type
+title: 'Using with @deepkit/type'
+---
+
+This guide shows how to use `typegoose` with `@deepkit/type`.
+
+## Implementation
+
+Suppose you have this `Account` class decorated with `@deepkit/type`:
+
+```ts
+import { classToPlain, t, plainToClass } from '@deepkit/type';
+import { getModelForClass, prop } from '../../src/typegoose';
+
+enum Group {
+    confidential = 'confidential',
+    public = 'public'
+}
+
+class Account {
+  @t.mongoId.group(Group.public)
+  public _id: string;
+
+  @t.group(Group.public)
+  public __v: number;
+
+  @t.group(Group.public)
+  @prop()
+  public email: string;
+  
+  @t.group(Group.confidential)
+  @prop()
+  public confidentialProperty?: string;
+}
+
+const AccountModel = getModelForClass(Account);
+```
+You can then use, for example:
+
+* `lean()` on the query:
+
+```ts
+  const pojo = await AccountModel.findById(id).orFail().lean().exec();
+  // groupsExclude option excludes the property group
+  const access = { groupsExclude: [Group.confidential]}
+  // deserialize pojo back to an Account instance
+  const deserialized = plainToClass(Account, pojo, access);
+ 
+  expect(deserialized).toStrictEqual(accountClassObject);
+```
+
+* or a normal document:
+
+```ts
+  // exec returns a Mongoose Object
+  const doc = await AccountModel.findById(id).orFail().exec();
+  const access = { groupsExclude: [Group.confidential]}
+  // serialize Account instance back to a Plain Old Javascript Object
+  const serialized = classToPlain(Account, doc, access);
+```
+
+As you can see from these code examples, there is:
+
+* an `access` or "grouping" definition needed to ouput the required properties for any serializing or deserializing. The @deepkit/type author suggests not using groupings at all, as they cause @deepkit/type's performance to deteriorate. 
+* a `mongo.id` decorator built into @deepkit/type's decorators. 
+
+The @deepkit/type's author also suggests not putting passwords or other confidential data directly inside entities that have mainly public access. It is better to put them in their own class or classes, so they can be handled without the grouping feature. This allows for the performance of serialization and deserialization to be much better and you can control who sees what more appropriately i.e. not via serialization steps. 
+
+Also, you'll only need `@t` to decorate your Entity, so your code will be cleaner too. This is what the `Account` entity would look like without the grouping/ access code. 
+
+```ts
+class Account {
+  @t.mongoId
+  public _id: string;
+
+  @t
+  public __v: number;
+
+  @t
+  @prop()
+  public email: string;
+  
+}
+```
+The `confidentialProperty` property would be in a different entity/ class. 
+
+Lastly, and an even better tip is to not use the entity class at all for the serialization and deserialization definitions (i.e. with decorator metadata). It is better to use [DTOs](https://en.wikipedia.org/wiki/Data_transfer_object) for this purpose.  
+---
+
+:::info
+For more information, you can always look at the [typegoose @deepkit/type tests](https://github.com/typegoose/typegoose/blob/master/test/tests/deepkitType.test.ts)
+:::
---
+++ test/tests/deepkitType.test.ts
@@ -0,0 +1,95 @@
+import { classToPlain, t, plainToClass } from '@deepkit/type';
+import { getModelForClass, prop } from '../../src/typegoose';
+
+enum Group {
+    confidential = 'confidential',
+    public = 'public'
+}
+
+class Account {
+  @t.mongoId.group(Group.public)
+  public _id: string;
+
+  @t.group(Group.public)
+  public __v: number;
+
+  @t.group(Group.public)
+  @prop()
+  public email: string;
+  
+  @t.group(Group.confidential)
+  @prop()
+  public confidentialProperty?: string;
+}
+
+const AccountModel = getModelForClass(Account);
+
+describe('@deepkit/type transforms', () => {
+  let id: string;
+  let accountClassObject: Account;
+  let account: Account;
+  beforeEach(async () => {
+    accountClassObject = new Account() 
+    accountClassObject.email = 'somebody@gmail.com';
+    accountClassObject.confidentialProperty = 'secret';
+    account = await AccountModel.create(accountClassObject);
+    // note here that _id is an ObjectId, hence the toString()
+    // otherwise it will have the shape of : { _bsonType: 'ObjectId', id: ArrayBuffer }
+    id = account._id;
+    accountClassObject._id = account._id
+    accountClassObject.__v = account.__v
+  });
+
+  describe('lean query', () => {
+    it(`should be able to use @deepkit/type to transform to class instance`, async () => {
+      // lean return a Plain Old Javascript Object
+      const pojo = await AccountModel.findById(id).orFail().lean().exec();
+      // groupsExclude option excludes the property group
+      const access = { groupsExclude: [Group.confidential]}
+      // deserialize pojo back to an Account instance
+      const deserialized = plainToClass(Account, pojo, access);
+      // need to remove the password property, as it is not available due to our @t.group confidential exclusion definition
+      delete accountClassObject.confidentialProperty
+      expect(deserialized).toStrictEqual(accountClassObject);
+    });
+
+    it(`should be able to use @deepkit/type's groups to allow access`, async () => {
+      // lean return a Plain Old Javascript Object
+      const pojo = await AccountModel.findById(id).orFail().lean().exec();
+      // groups option includes the property group
+      const access = { groups: [Group.confidential, Group.public] };
+      // deserialize pojo back to an Account instance
+      const deserialized = plainToClass(Account, pojo , access);
+      expect(deserialized).toStrictEqual(accountClassObject);
+    });
+  });
+
+  describe('normal query', () => {
+    it('should be able to use @deepkit/type transform', async () => {
+      // exec returns a Mongoose Object
+      const doc = await AccountModel.findById(id).orFail().exec();
+      const access = { groupsExclude: [Group.confidential]}
+      // serialize Account instance back to a Plain Old Javascript Object
+      const serialized = classToPlain(Account, doc, access);
+      expect(serialized).toStrictEqual({
+        _id: id,
+        __v: 0,
+        email: 'somebody@gmail.com',
+      });
+    });
+
+    it(`should be able to use @deepkit/type's groups to allow access`, async () => {
+      // exec return a Mongoose Object
+      const doc = await AccountModel.findById(id).orFail().exec();
+      const access = { groups: [Group.confidential, Group.public] };
+      // serialize Account instance back to a Plain Old Javascript Object
+      const serialized = classToPlain(Account, doc, access);
+      expect(serialized).toStrictEqual({
+        _id: id,
+        __v: 0,
+        email: 'somebody@gmail.com',
+        'confidentialProperty': 'secret',
+      });
+    });
+  });
+});
