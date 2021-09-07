1a918ca8cd70695877540de9033b014a79797a54
hasezoey <hasezoey@gmail.com>
2021-09-07 16:48:31 +0200

    docs(sidebars): update to look better

--- website/sidebars.js
+++ website/sidebars.js
@@ -25,27 +25,35 @@ module.exports = {
     Types: ['api/types/document-type', 'api/types/return-model-type', 'api/types/ref-type', 'api/types/additional-types'],
   },
   guides: {
-    'Getting Started': [
-      'guides/quick-start-guide',
-      'guides/faq',
-      'guides/known-issues',
-      'guides/deprecation-codes',
-      'guides/error-warning-details',
-    ],
-    Basics: ['guides/all-decorators', 'guides/motivation', 'guides/default-classes', 'guides/use-without-emitDecoratorMetadata'],
-    Advanced: [
+    'Getting Started': ['guides/quick-start-guide', 'guides/faq', 'guides/known-issues'],
+    // 'guides/known-issues' is included in both "Getting Started" and "Guides", because it fits into both categories
+    Guides: [
+      {
+        'Integration Examples': [
+          'guides/advanced/using-with-class-transformer',
+          'guides/advanced/using-with-deepkit-type',
+          'guides/advanced/common-plugins',
+        ],
+        Miscellaneous: [
+          'guides/advanced/logger',
+          'guides/mongoose-compatibility',
+          'guides/all-decorators',
+          'guides/deprecation-codes',
+          'guides/error-warning-details',
+          'guides/known-issues',
+          'guides/use-without-emitDecoratorMetadata',
+          'guides/motivation',
+        ],
+      },
+      'guides/default-classes',
       'guides/advanced/custom-types',
       'guides/advanced/array-types',
       'guides/advanced/models-with-same-name',
-      'guides/advanced/logger',
       'guides/advanced/reference-other-classes',
-      'guides/advanced/common-plugins',
       'guides/advanced/change-id-type',
       'guides/advanced/using-objectid-type',
-      'guides/advanced/using-with-class-transformer',
-      'guides/advanced/using-with-deepkit-type',
       'guides/advanced/non-nested-discriminators',
     ],
-    Migration: ['guides/mongoose-compatibility', 'guides/migrate-9', 'guides/migrate-8', 'guides/migrate-7', 'guides/migrate-6'],
+    Migration: ['guides/migrate-9', 'guides/migrate-8', 'guides/migrate-7', 'guides/migrate-6'],
   },
 };
