b4e8ec8f5cc788881c05f765e8a1695a157002d5
hasezoey <hasezoey@gmail.com>
2022-02-05 12:26:24 +0100

    dependencies(mongoose): upgrade to 6.2.0

--- src/typegoose.ts
+++ src/typegoose.ts
@@ -8,8 +8,8 @@ import { assertion, assertionIsClass, getName, isNullOrUndefined, mergeMetadata,
 if (!isNullOrUndefined(process?.version) && !isNullOrUndefined(mongoose?.version)) {
   // for usage on client side
   /* istanbul ignore next */
-  if (semver.lt(mongoose?.version, '6.1.6')) {
-    throw new Error(`Please use mongoose 6.1.6 or higher (Current mongoose: ${mongoose.version}) [E001]`);
+  if (semver.lt(mongoose?.version, '6.2.0')) {
+    throw new Error(`Please use mongoose 6.2.0 or higher (Current mongoose: ${mongoose.version}) [E001]`);
   }
 
   /* istanbul ignore next */
