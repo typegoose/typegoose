54b11ea0aaa336b9cab831f6177c261d801d625b
Lioness100 <jchickmm2@gmail.com>
2022-08-24 15:18:34 -0400

    fix: properly escape quotes

--- src/internal/errors.ts
+++ src/internal/errors.ts
@@ -110,7 +110,7 @@ export class OptionDoesNotSupportOptionError extends TypeError {
 export class ResolveTypegooseNameError extends ReferenceError {
   constructor(input: unknown) {
     super(
-      'Input was not a string AND didn't have a .typegooseName function AND didn't have a .typegooseName string [E014]\n' +
+      "Input was not a string AND didn't have a .typegooseName function AND didn't have a .typegooseName string [E014]\n" +
         `Value: "${toStringNoFail(input)}"`
     );
   }
