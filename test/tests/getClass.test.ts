bd2543a9e4a886606260494d0c0da80d65b6d79a
hasezoey <hasezoey@gmail.com>
2022-11-27 13:03:13 +0100

    test: move unrelated tests from "getClass" to "shouldRun"

--- test/tests/getClass.test.ts
+++ test/tests/getClass.test.ts
@@ -1,8 +1,6 @@
 import * as mongoose from 'mongoose';
 import { getClass, isDocument } from '../../src/typegoose';
 import { Car, CarModel } from '../models/car';
-import { InternetUserModel } from '../models/internetUser';
-import { AddressNested, AddressNestedModel, PersonNested, PersonNestedModel } from '../models/nestedObject';
 import { PersonModel } from '../models/person';
 import { Genders, User, UserModel } from '../models/user';
 
@@ -56,82 +54,4 @@ it('should use inherited schema', async () => {
   // verify methods
   expect(user.getClassName()).toEqual('Person');
   expect(PersonModel.getStaticName()).toEqual('Person');
-});
-
-it('should store nested address', async () => {
-  const person = await PersonNestedModel.create({
-    name: 'Person, Some',
-    address: new AddressNestedModel({ street: 'A Street 1' } as AddressNested),
-    moreAddresses: [
-      new AddressNestedModel({ street: 'A Street 2' } as AddressNested),
-      new AddressNestedModel({ street: 'A Street 3' } as AddressNested),
-    ],
-  } as PersonNested);
-
-  expect(person).not.toBeUndefined();
-  expect(person.name).toEqual('Person, Some');
-  expect(person.address).not.toBeUndefined();
-  expect(person.address!.street).toEqual('A Street 1');
-  expect(person.moreAddresses).not.toBeUndefined();
-  expect(person.moreAddresses!.length).toEqual(2);
-  expect(person.moreAddresses![0].street).toEqual('A Street 2');
-  expect(person.moreAddresses![1].street).toEqual('A Street 3');
-});
-
-it('should properly set Decimal128, ObjectID types to field', () => {
-  expect((CarModel.schema as any).paths.price.instance).toEqual('Decimal128');
-  expect((CarModel.schema as any).paths.someId.instance).toEqual('ObjectID');
-});
-
-// failed validation will need to be checked
-it('should validate Decimal128', async () => {
-  expect.assertions(3);
-  try {
-    await CarModel.create({
-      carModel: 'Tesla',
-      price: 'NO DECIMAL',
-    });
-    fail('Validation must fail.');
-  } catch (e) {
-    expect(e).toBeInstanceOf(mongoose.Error.ValidationError);
-  }
-  const car = await CarModel.create({
-    carModel: 'Tesla',
-    price: mongoose.Types.Decimal128.fromString('123.45'),
-  });
-  const foundCar = await CarModel.findById(car._id).orFail().exec();
-  expect(foundCar.price).toBeInstanceOf(mongoose.Types.Decimal128);
-  expect(foundCar.price.toString()).toEqual('123.45');
-});
-
-it('should validate email', async () => {
-  try {
-    await PersonModel.create({
-      email: 'email',
-    });
-    fail('Validation must fail.');
-  } catch (e) {
-    expect(e).toBeInstanceOf(mongoose.Error.ValidationError);
-    expect(e.message).toEqual(
-      // test it specifically, to know that it is not another error
-      'Person validation failed: email: Validator failed for path `email` with value `email`'
-    );
-  }
-});
-
-it(`should Validate Map`, async () => {
-  try {
-    await InternetUserModel.create({
-      projects: {
-        p1: 'project',
-      },
-    });
-    fail('Validation should Fail');
-  } catch (e) {
-    expect(e).toBeInstanceOf(mongoose.Error.ValidationError);
-    expect(e.message).toEqual(
-      // test it specifically, to know that it is not another error
-      'InternetUser validation failed: projects.p1: `project` is not a valid enum value for path `projects.p1`.'
-    );
-  }
 });
