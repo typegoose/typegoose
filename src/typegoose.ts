e0374bd422ece10393860247b50c5b6ba8ce7b3a
hasezoey <hasezoey@gmail.com>
2023-06-09 21:29:32 +0200

    dependencies(mongoose): upgrade to 7.2.3
    fixes typegoose/typegoose#846

--- src/typegoose.ts
+++ src/typegoose.ts
@@ -19,8 +19,8 @@ import {
 if (typeof process !== 'undefined' && !isNullOrUndefined(process?.version) && !isNullOrUndefined(mongoose?.version)) {
   // for usage on client side
   /* istanbul ignore next */
-  if (semver.lt(mongoose?.version, '7.2.0')) {
-    throw new Error(`Please use mongoose 7.2.0 or higher (Current mongoose: ${mongoose.version}) [E001]`);
+  if (semver.lt(mongoose?.version, '7.2.3')) {
+    throw new Error(`Please use mongoose 7.2.3 or higher (Current mongoose: ${mongoose.version}) [E001]`);
   }
 
   /* istanbul ignore next */
