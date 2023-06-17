0b063404b7282c4714c8e6c1bc14cc5294c0bf6b
hasezoey <hasezoey@gmail.com>
2023-06-17 16:13:01 +0200

    chore: make use of "deployInfo.searchName"

--- website/src/theme/SearchMetadata/index.js
+++ website/src/theme/SearchMetadata/index.js
@@ -5,11 +5,9 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 export default function SearchMetadata({ locale, tag }) {
   const { siteConfig } = useDocusaurusContext();
   const deployInfo = siteConfig.customFields?.deployInfo ?? {};
-  // if "deployPath" is empty, use deployName "current" and tag "current"
-  const deployPath = deployInfo?.deployPath ?? '';
-  const deployName = deployPath.length === 0 ? 'current' : deployInfo?.deployName ?? 'current';
+  const searchName = deployInfo?.searchName ?? 'current';
 
-  tag = tag.replace('current', deployName); // replace "docs-default-current" with something like "docs-default-YourVersion"
+  tag = tag.replace('current', searchName); // replace "docs-default-current" with something like "docs-default-YourVersion"
 
   return (
     <Head>
@@ -18,12 +16,12 @@ export default function SearchMetadata({ locale, tag }) {
         See https://github.com/cmfcmf/docusaurus-search-local/issues/99
         */}
       {locale && <meta name="docusaurus_locale" content={locale} />}
-      {<meta name="docusaurus_version" content={deployName} />}
+      {<meta name="docusaurus_version" content={searchName} />}
       {tag && <meta name="docusaurus_tag" content={tag} />}
 
       {/* Algolia DocSearch metadata */}
       {locale && <meta name="docsearch:language" content={locale} />}
-      {<meta name="docsearch:version" content={deployName} />}
+      {<meta name="docsearch:version" content={searchName} />}
       {tag && <meta name="docsearch:docusaurus_tag" content={tag} />}
     </Head>
   );
