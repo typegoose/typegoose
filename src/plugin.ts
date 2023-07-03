40a69b323abf0b5bd0bff0a06c2403c0f934a548
hasezoey <hasezoey@gmail.com>
2023-07-03 21:23:41 +0200

    fix: wrong plugin function

--- src/plugin.ts
+++ src/plugin.ts
@@ -20,7 +20,7 @@ export function plugin<TFunc extends Func, TParams = Parameters<TFunc>[1]>(
   // don't check if options is an object, because any plugin could make it anything
   return wrapClassDecorator(({ metadata, className }) => {
     logger.info('Adding plugin "%s" to "%s" with options: "%o"', mongoosePlugin?.name || '<anonymous>', className, options);
-    const plugins: IPluginsArray[] = Array.from((metadata.getOwnMetadata(DecoratorKeys.Plugins) as IPluginsArray[] | undefined) ?? []);
+    const plugins: IPluginsArray[] = Array.from((metadata.getMetadata(DecoratorKeys.Plugins) as IPluginsArray[] | undefined) ?? []);
     plugins.push({ mongoosePlugin, options });
     metadata.defineMetadata(DecoratorKeys.Plugins, plugins);
   });
