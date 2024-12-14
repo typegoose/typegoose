b3dbe1169dfe833f8dcba04a1ab3bedaaf669da0
hasezoey <hasezoey@gmail.com>
2024-12-14 17:57:49 +0100

    fix(logSettings): re-export used functions explicitly
    otherwise jest is unable to mock functions when "esModuleInterop: true"
    and "_logger" is not default imported.
    
    This is not a breaking change as other functions from "loglevel"
    were not expected to be used through the typegoose import.

--- src/logSettings.ts
+++ src/logSettings.ts
@@ -1,7 +1,19 @@
-import * as logger from 'loglevel';
+import * as _logger from 'loglevel';
 
-export { logger };
+// this has to be re-defined as otherwise jest is not able to mock any function
+// when "esModuleInterop: true" and "_logger" is not default imported
+// and "_logger" cannot be default imported as that would make all projects using typegoose require to set "allowSyntheticDefaultImports"
+export const logger = {
+  warn: _logger.warn,
+  error: _logger.error,
+  info: _logger.info,
+  debug: _logger.debug,
+  setLogLevel: _logger.setLevel,
+  LogLevels: _logger.levels,
+  setDefaultLevel: _logger.setDefaultLevel,
+  _logger: _logger,
+};
 
-export const setLogLevel = logger.setLevel;
-export const LogLevels = logger.levels;
-logger.setDefaultLevel(LogLevels.WARN);
+export const setLogLevel = logger.setLogLevel;
+export const LogLevels = logger.LogLevels;
+logger.setDefaultLevel(logger.LogLevels.WARN);
