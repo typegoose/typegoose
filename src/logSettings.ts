e406af2f9ab7c76f67ba2c759e90a9d7fcd45b9f
hasezoey <hasezoey@gmail.com>
2025-02-08 13:27:06 +0100

    fix(logSettings): actually allow more levels than "warn" and "error"
    "loglevel" is self-modifying, requiring getter functions to get the correct function at the time.

--- src/logSettings.ts
+++ src/logSettings.ts
@@ -4,10 +4,12 @@ import * as _logger from 'loglevel';
 // when "esModuleInterop: true" and "_logger" is not default imported
 // and "_logger" cannot be default imported as that would make all projects using typegoose require to set "allowSyntheticDefaultImports"
 export const logger = {
-  warn: _logger.warn,
-  error: _logger.error,
-  info: _logger.info,
-  debug: _logger.debug,
+  // "loglevel" here referred as "_logger" is self-modifying, so a getter function is necessary to actually get logs
+  // otherwise we only get the default "noop" function forever
+  warn: (...args) => _logger.warn.apply(undefined, args),
+  error: (...args) => _logger.error.apply(undefined, args),
+  info: (...args) => _logger.info.apply(undefined, args),
+  debug: (...args) => _logger.debug.apply(undefined, args),
   setLogLevel: _logger.setLevel,
   LogLevels: _logger.levels,
   setDefaultLevel: _logger.setDefaultLevel,
